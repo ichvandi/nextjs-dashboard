0929bbd375c0ffc6f17dfafa087e640ef6053092
Ichvandi Okta Maulana <ichvandi.maulana@stockbit.com>
2023-11-01 20:09:43 +0700

    End chapter 11

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -135,7 +135,7 @@ export async function fetchFilteredInvoices(
 
 export async function fetchInvoicesPages(query: string) {
   noStore();
-
+  // await new Promise((resolve) => setTimeout(resolve, 1000));
   try {
     const count = await sql`SELECT COUNT(*)
     FROM invoices
